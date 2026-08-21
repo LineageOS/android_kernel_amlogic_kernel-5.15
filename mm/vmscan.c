khadas/linux

mm: add memory extend support. [2/2]
PD#SWPL-73189

problem:
add memory extend support.

Solution:
mm: reduce dcache and inode hash size. [1/1]
dma-buf: dma-buf system heap report warning. [1/1]
gki: delete vendor modifications to meet gki requirements. [1/1]
mm: add AMLOGIC_MEMORY_STAT config. [1/2]
mm: print more information for slabinfo. [1/1]
mm: add code to count free pages on migrate list [1/1]
mm: fixed tvp alloc failed. [1/1]
reserved_memory: change print of all reserved memory. [1/1]
dma-buf: dma-buf system heap report error [1/1]
mm: reduce cache line size to 64 bytes to save memory [1/1]
mm: forward memory reclaim process [2/2]

Verify:
local.

Change-Id: I5a67b958824bc85c75fba2c9a5e726ffe4555343
Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

mm/vmscan.c
@@ -1404,6 +1404,17 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 	return nr_succeeded;
 }
 
+#ifdef CONFIG_AMLOGIC_MEMORY_EXTEND
+static int can_unmap_files(struct scan_control *sc, struct page *page)
+{
+	if (!current_is_kswapd())
+		return 1;
+	if (page_mapcount(page) > 1)
+		return 0;
+	return 1;
+}
+#endif
+
 /*
  * shrink_page_list() returns the number of reclaimed pages
  */
@@ -1450,7 +1461,11 @@ retry:
 		if (unlikely(!page_evictable(page)))
 			goto activate_locked;
 
+	#ifdef CONFIG_AMLOGIC_MEMORY_EXTEND
+		if (!can_unmap_files(sc, page))
+	#else
 		if (!sc->may_unmap && page_mapped(page))
+	#endif
 			goto keep_locked;
 
 		/* page_update_gen() tried to promote this page? */
@@ -2156,8 +2171,10 @@ static int too_many_isolated(struct pglist_data *pgdat, int file,
 	 * won't get blocked by normal direct-reclaimers, forming a circular
 	 * deadlock.
 	 */
+#ifndef CONFIG_AMLOGIC_MEMORY_EXTEND
 	if ((sc->gfp_mask & (__GFP_IO | __GFP_FS)) == (__GFP_IO | __GFP_FS))
 		inactive >>= 3;
+#endif
 
 #ifdef CONFIG_AMLOGIC_CMA
 	check_cma_isolated(&isolated, inactive, inactive);

LineageOS/android_kernel_amlogic_kernel-5.15

mm/vmscan.c
@@ -1411,6 +1411,17 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 	return nr_succeeded;
 }
 
+#ifdef CONFIG_AMLOGIC_SWAPD_OPT
+static int can_unmap_files(struct scan_control *sc, struct page *page)
+{
+	if (!current_is_kswapd())
+		return 1;
+	if (page_mapcount(page) > 1)
+		return 0;
+	return 1;
+}
+#endif
+
 /*
  * shrink_page_list() returns the number of reclaimed pages
  */
@@ -1457,7 +1468,11 @@ retry:
 		if (unlikely(!page_evictable(page)))
 			goto activate_locked;
 
+	#ifdef CONFIG_AMLOGIC_SWAPD_OPT
+		if (!can_unmap_files(sc, page))
+	#else
 		if (!sc->may_unmap && page_mapped(page))
+	#endif
 			goto keep_locked;
 
 		/* page_update_gen() tried to promote this page? */
@@ -2163,8 +2178,10 @@ static int too_many_isolated(struct pglist_data *pgdat, int file,
 	 * won't get blocked by normal direct-reclaimers, forming a circular
 	 * deadlock.
 	 */
+#ifndef CONFIG_AMLOGIC_MEMORY_EXTEND
 	if ((sc->gfp_mask & (__GFP_IO | __GFP_FS)) == (__GFP_IO | __GFP_FS))
 		inactive >>= 3;
+#endif
 
 #ifdef CONFIG_AMLOGIC_CMA
 	check_cma_isolated(&isolated, inactive, inactive);